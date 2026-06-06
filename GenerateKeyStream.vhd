b51eaed3f50c91feeebefa5a48ba868086370f71
FranCagliari <franchescopc@outlook.com>
2026-06-06 16:14:20 -0300

    Arch Behavioral GenerateKeyStream

--- GenerateKeyStream.vhd
+++ GenerateKeyStream.vhd
@@ -56,7 +56,76 @@ begin
 		);
 end structural;
 
---architecture behavioral of GenerateKeyStream is
-	-- Parte 2 do Trabalho
---begin
---end behavioral;
+architecture behav of GenerateKeyStream is --ESBOÇO, nao testado
+	
+	type State is (S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12);
+	signal currentState: State;
+
+	-- sinais para registradores 
+	signal state, stateSize, textSize, keyStream, dataIn, I, J, K, temp: std_logic_vector(DATAWIDTH-1 downto 0); 
+	signal count, arrayEnd: std_logic_vector(DATA_WIDTH-1 downto 0);
+
+begin
+	process(clk, rst)
+	begin 
+	  
+	if rst = '1' then 
+       	  currentState <= S0;
+
+	elsif rising_edge(clk) then
+
+	   case currentState is 
+	 	when S0 =>
+	
+		  if data_av = '1' then	
+		
+			state <= data;
+			currentState <= S1;
+		  end if; 			
+	
+		when S1 =>
+		  
+		  if data_av = '1' then	
+		
+			stateSize <= data;
+			currentState <= S2; 
+		  end if; 
+		 
+		when S2 =>
+		  
+		  if data_av = '1' then	
+		
+			textSize <= data;
+			currentState <= S3;
+		  end if;  
+
+		when S3 =>
+		  
+		  if data_av = '1' then	
+		
+			keyStream <= data;
+			K <= data;
+			currentState <= S4;
+			arrayEnd <= keyStream + textSize;
+			
+		  end if;
+	  
+		when S4 =>
+			
+		  if K < arrayEnd then
+			currentState <= S5;
+			
+
+
+		when S5 =>
+		when S6 =>
+		when S7 =>
+		when S8 =>
+		when S9 =>
+		when S10 =>
+		when S11 =>
+		when S12 =>
+
+
+
+end behav;
